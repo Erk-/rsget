1457edee258a8091a8f3c78c26cea648482a7431
Valdemar Erk <v@erk.io>
2018-04-19 15:29:52 +0000

    Added player option

--- rsget_cli/src/main.rs
+++ rsget_cli/src/main.rs
@@ -8,6 +8,7 @@ extern crate tokio_core;
 use rsget_lib::Streamable;
 use clap::{App, Arg}; //, SubCommand};
 use tokio_core::reactor::Core;
+use std::process::Command;
 
 fn main() {
     env_logger::init();
@@ -17,8 +18,14 @@ fn main() {
         .author("Valdemar Erk <v@erk.io>")
         .about("Downloads streams")
         .arg(
+            Arg::with_name("play")
+                .short("P")
+                .long("play")
+                .takes_value(false),
+        )
+        .arg(
             Arg::with_name("path")
-                .short("p")
+                .short("O")
                 .long("path")
                 .takes_value(true),
         )
@@ -48,6 +55,15 @@ fn main() {
     if !stream.is_online() {
         info!("Stream not online");
         std::process::exit(1)
+    }
+
+    if matches.is_present("player") {
+        let status = Command::new("mpv")
+            .arg("--no-ytdl")
+            .arg(stream.get_stream())
+            .status()
+            .expect("Mpv failed to start");
+        std::process::exit(status.code().unwrap())
     }
 
     let path = String::from(matches.value_of("path").unwrap_or("./"));
