809255e4dacb43d69cad364ff1ab06341abdcdf0
Valdemar Erk <valdemar@erk.io>
2020-10-03 14:44:17 +0200

    Port the vlive plugin to async
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- rsget_lib/src/plugins/mod.rs
+++ rsget_lib/src/plugins/mod.rs
@@ -2,11 +2,13 @@ mod afreeca;
 mod bilibili;
 mod dlive;
 mod twitch;
+mod vlive;
 
 pub use afreeca::Afreeca;
 pub use bilibili::Bilibili;
 pub use dlive::DLive;
 pub use twitch::Twitch;
+pub use vlive::Vlive;
 
 /*
 pub mod douyin;
@@ -14,5 +16,4 @@ pub mod douyu;
 pub mod huya;
 pub mod inke;
 pub mod tiktok;
-pub mod vlive;
 */
